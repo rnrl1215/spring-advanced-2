37326f3096e5965e316e3addea39b019fae90e76
skahn <d1215@naver.com>
2022-12-29 23:04:26 +0900

    first commit

---
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1Impl.java
@@ -0,0 +1,13 @@
+package hello.proxy.app.v1;
+
+public class OrderControllerV1Impl implements OrderControllerV1{
+    @Override
+    public String request(String itemId) {
+        return null;
+    }
+
+    @Override
+    public String noLog() {
+        return null;
+    }
+}
