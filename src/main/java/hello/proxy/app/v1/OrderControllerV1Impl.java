0a5b8e718d8efe365198e7df76dfbc1b3800b6b7
skahn <d1215@naver.com>
2022-12-30 22:02:13 +0900

    feat: Modify code

--- src/main/java/hello/proxy/app/v1/OrderControllerV1Impl.java
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1Impl.java
@@ -1,9 +1,17 @@
 package hello.proxy.app.v1;
 
 public class OrderControllerV1Impl implements OrderControllerV1{
+
+    private final OrderServiceV1 orderService;
+
+    public OrderControllerV1Impl(OrderServiceV1 orderService) {
+        this.orderService = orderService;
+    }
+
     @Override
     public String request(String itemId) {
-        return null;
+        orderService.orderItem(itemId);
+        return "ok";
     }
 
     @Override
--- src/main/java/hello/proxy/app/v1/OrderServiceV1Impl.java
+++ src/main/java/hello/proxy/app/v1/OrderServiceV1Impl.java
@@ -1,14 +1,14 @@
 package hello.proxy.app.v1;
 
 public class OrderServiceV1Impl implements OrderServiceV1{
-    private final  OrderRepositoryV1 orderRepositoryV1;
+    private final  OrderRepositoryV1 orderRepository;
 
-    public OrderServiceV1Impl(OrderRepositoryV1 orderRepositoryV1) {
-        this.orderRepositoryV1 = orderRepositoryV1;
+    public OrderServiceV1Impl(OrderRepositoryV1 orderRepository) {
+        this.orderRepository = orderRepository;
     }
 
     @Override
     public void orderItem(String itemId) {
-        orderRepositoryV1.save(itemId);
+        orderRepository.save(itemId);
     }
 }
