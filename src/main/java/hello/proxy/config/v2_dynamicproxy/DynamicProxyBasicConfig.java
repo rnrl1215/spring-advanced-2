dfbd176eb06d20dc8edf630ef26012d34ad8094b
skahn <d1215@naver.com>
2023-01-14 16:06:45 +0900

    refacotr: Refactoring code

--- src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyBasicConfig.java
+++ src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyBasicConfig.java
@@ -4,11 +4,9 @@ package hello.proxy.config.v2_dynamicproxy;
 import hello.proxy.app.v1.*;
 import hello.proxy.config.v2_dynamicproxy.handler.LogTraceBasicHandler;
 import hello.proxy.trace.logtrace.LogTrace;
-import lombok.extern.java.Log;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.swing.plaf.SpinnerUI;
 import java.lang.reflect.Proxy;
 
 @Configuration
